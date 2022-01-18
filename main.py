02df810432e25d1cf797e035ed529f46a8cf6ee9
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-18 06:02:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Assume that the variable data refers to the string "myprogram.exe". Write the values of the following expressions
+# A. data[2] 
+# B. data[-1] 
+# C. len(data) 
+# D. data[0:8] 
+
+data="myprogram.exe"
+print(data[2])
+print(data[-1])
+print(len(data))
+print(data[0:8])
+
+
